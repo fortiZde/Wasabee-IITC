20b6f34a3162590112ed3b7c86043ac44798e91d
fortiZde <github@systemkaiser.de>
2026-04-30 07:11:49 +0000

    Deploying to dist from @ fortiZde/Wasabee-IITC@0ef64e72f1759f8d84c66300923a18f8ac3c27c6 🚀

---
+++ dev/wasabee.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @id            Wasabee.user.js
+// @name          IITC plugin: Wasabee Draw Tools
+// @namespace     https://wasabee.rocks/
+// @version       0.22.20260430071140-dev-0ef64e7
+// @updateURL     https://cdn2.wasabee.rocks/iitcplugin/latest/dev/wasabee.meta.js
+// @downloadURL   https://cdn2.wasabee.rocks/iitcplugin/latest/dev/wasabee.user.js
+// @description   Wasabee is not for dating.
+// @author        Wasabee Project Team
+// @match         https://intel.ingress.com/*
+// @category      Draw
+// @grant         none
+// ==/UserScript==
+
